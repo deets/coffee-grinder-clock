d9c36fa4b48815ba9d545956818b2e4bb20e35a9
Diez B. Roggisch <deets@web.de>
2020-07-26 20:19:29 +0200

    Use MPU6050 for the first time

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,7 @@
 #include "display.hh"
 #include "buzzer.hh"
+#include "i2c.hh"
+#include "mpu6050.hh"
 
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
@@ -10,11 +12,16 @@
 
 extern "C" void app_main();
 
+#define MPU6050_ADDRESS_AD0_LOW 0x68
+#define MPU6050_ADDRESS_AD0_HI 0x69
+#define MPU6050_RA_WHO_AM_I 0x75
+
 namespace {
 
 const int MAINLOOP_WAIT = 16; // 60fps
 const int WIFI_WAIT = 500;
-
+const auto SDA = gpio_num_t(18);
+const auto SCL = gpio_num_t(21);
 
 
 } // end ns anon
@@ -25,12 +32,21 @@ void app_main()
 
   Display display;
   Buzzer buzzer(static_cast<gpio_num_t>(14));
+  I2CHost i2c(I2C_NUM_0, SDA, SCL);
+  MPU6050 mpu(
+    MPU6050_ADDRESS_AD0_HI,
+    i2c,
+    MPU6050::GYRO_250_FS,
+    MPU6050::ACC_2_FS
+    );
 
-  std::array<char, 200> distance_text;
   for( ;; )
   {
 
     vTaskDelay(pdMS_TO_TICKS(MAINLOOP_WAIT));
+
+    const auto gyro_data = mpu.read_raw();
+    ESP_LOGI("mpu", "gyros: %f %f %f", gyro_data.gyro[0], gyro_data.gyro[1], gyro_data.gyro[2]);
     display.clear();
     display.font_render(
       SMALL,
